65938d2ee9db7c25fa4d4ad49fa87feb302fa836
joe <joe@fuelwell.com>
2015-07-29 12:26:09 -0500

    freeze2

--- js/scripts.js
+++ js/scripts.js
@@ -137,7 +137,7 @@ var appMaster = {
     },
     placeHold: function(){
         // run Placeholdem on all elements with placeholders
-        Placeholdem(document.querySelectorAll('[placeholder]'));
+        //Placeholdem(document.querySelectorAll('[placeholder]'));
     }
 
 }; // AppMaster
