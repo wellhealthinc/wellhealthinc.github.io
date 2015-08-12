6de01c6dfa27fee408e40eab580a94e71d5447bf
joe <joe@fuelwell.com>
2015-08-11 18:52:24 -0700

    asdfasdf

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,49 +8,40 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
+            
             var name = $("input#name").val();
+            
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
+            var cell = $("input#cell").val();
+            /*
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
+
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+            */
+ $.ajax({
+        url: 'https://api.parse.com/1/functions/sendProviderDemoEmail',
+        type: 'POST',
+        headers: {
+          'X-Parse-Application-Id': 'WcNXULqtiPB7RRu2edl4UGsCS5ZFeguwobEi3PDg',
+          'X-Parse-REST-API-Key': 'eOPBrESbJ8MGSYGWskcFQPcdtuuwg9y76esoOaWK'
+        },
+        data: {
+          providerName: $('input#name').val(),
+          providerEmailAddress: $('input#email').val()
+        },
+        dataType: 'json',
+        success: function (json) {
+          alert("Success!", json);
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+        },
+        error: function (xhr, status, errorThrown) {
+          alert("error!", status, errorThrown);
+        }
+      });
         },
         filter: function() {
             return $(this).is(":visible");
