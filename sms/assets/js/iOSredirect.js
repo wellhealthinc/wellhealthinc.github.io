c5d971c75c923d4d60ff32878dd772d13912bb1c
joe <joe@fuelwell.com>
2015-09-01 15:10:43 -0500

    update sms redirect

--- sms/assets/js/iOSredirect.js
+++ sms/assets/js/iOSredirect.js
@@ -37,33 +37,27 @@ $(function() {
     if (/iPad|iPhone|iPod/.test(navigator.platform)) {
         console.log('iOS');
         $("#cover").fadeOut(200);
-        if (varTF !="" && varTF !=null){
-            var iOS = "https://wellhealth.typeform.com/to/" + varTF + "?" + varCFNP + "&" + varCLNP + "&" + varPIDP + "&" + varCIDP + "&" + varPNP + "&" + varFAIDP + "&os=1";        
-            
-        }else{
-            var iOS = "https://wellhealth.typeform.com/to/e7n17n?" + varCFNP + "&" + varCLNP + "&" + varPIDP + "&" + varCIDP + "&" + varPNP + "&" + varFAIDP + "&os=1";        
-            
-        }
 		
 		
     }else if (window.mobilecheck){
         console.log('Mobile, not iOS');
-        //$("#cover").fadeOut(200);
-        if (varTF !="" && varTF !=null){
+        $("#cover").fadeOut(200);
+        /*if (varTF !="" && varTF !=null){
             location.href = "https://wellhealth.typeform.com/to/" + varTF + "?" + varCFNP + "&" + varCLNP + "&" + varPIDP + "&" + varCIDP + "&" + varPNP + "&" + varFAIDP + "&os=0";         
         }else{
             location.href = "https://wellhealth.typeform.com/to/e7n17n?" + varCFNP + "&" + varCLNP + "&" + varPIDP + "&" + varCIDP + "&" + varPNP + "&" + varFAIDP + "&os=0";         
-        }
+        }*/
 
 
     }else{
     	console.log('Not Mobile, Not iOS');
+        $("#cover").fadeOut(200);
         //$("#cover").fadeOut(200);
-        if (varTF !="" && varTF !=null){
+        /*if (varTF !="" && varTF !=null){
             location.href = "https://wellhealth.typeform.com/to/" + varTF + "?" + varCFNP + "&" + varCLNP + "&" + varPIDP + "&" + varCIDP + "&" + varPNP + "&" + varFAIDP + "&os=2";
         }else{
             location.href = "https://wellhealth.typeform.com/to/e7n17n?" + varCFNP + "&" + varCLNP + "&" + varPIDP + "&" + varCIDP + "&" + varPNP + "&" + varFAIDP + "&os=2";
-        }
+        }*/
     	
     }
 
